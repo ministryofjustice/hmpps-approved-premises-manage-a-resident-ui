27a53ad542b454d555b5d7ff3709a8bed1d50ed7
robanderson-moj <rob.anderson1@justice.gov.uk>
2025-11-14 15:33:05 +0000

    Changed applicationInfo initialisation

--- server/applicationInfo.ts
+++ server/applicationInfo.ts
@@ -1,5 +1,4 @@
 import fs from 'fs'
-import path from 'path'
 import config from './config'
 
 const { buildNumber, gitRef, productId, branchName } = config
@@ -13,8 +12,15 @@ export type ApplicationInfo = {
   branchName: string
 }
 
-export default (): ApplicationInfo => {
-  const packageJson = path.join(__dirname, '../package.json')
-  const { name: applicationName } = JSON.parse(fs.readFileSync(packageJson).toString())
-  return { applicationName, buildNumber, gitRef, gitShortHash: gitRef.substring(0, 7), productId, branchName }
+const { name: applicationName } = JSON.parse(fs.readFileSync('./package.json').toString())
+
+const applicationInfo: ApplicationInfo = {
+  applicationName,
+  buildNumber,
+  gitRef,
+  gitShortHash: gitRef.substring(0, 7),
+  productId,
+  branchName,
 }
+
+export { applicationInfo }
--- server/services/index.ts
+++ server/services/index.ts
@@ -4,10 +4,9 @@ import ExampleService from './exampleService'
 import PlacementService from './placementService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, manageAResidentClient } = dataAccess()
+  const { hmppsAuditClient, manageAResidentClient } = dataAccess()
 
   return {
-    applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
     exampleService: new ExampleService(manageAResidentClient),
     placementService: new PlacementService(manageAResidentClient),
